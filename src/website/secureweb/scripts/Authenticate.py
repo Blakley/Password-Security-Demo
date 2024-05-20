4bf6997225ac6019ce59f79a27e5dd490cae9065
Anthony Blakley <blakley.ent@gmail.com>
2024-05-20 02:15:18 -0500

    Started implementing login form security checks

--- src/website/secureweb/scripts/Authenticate.py
+++ src/website/secureweb/scripts/Authenticate.py
@@ -2,3 +2,15 @@
 # Configuration for login form(s) security
 # =========================================
 
+import logging
+from datetime import datetime, timedelta
+
+# 
+locked_accounts = []
+locked_account_attempts = {} 
+locked_account_threshold = 5
+
+#
+black_listed_ips = []
+
+
